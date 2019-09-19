2db659cc7463de66a19cd688753c59e738f2e9f9
Michal Michalczuk <michalczukm@gmail.com>
2019-09-20 00:10:25 +0200

    update packages and make output pretty

--- 3_http-calls/index.js
+++ 3_http-calls/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const axios = require('axios');
-const { Transform } = require('stream');
+const express = require("express");
+const axios = require("axios");
+const { Transform } = require("stream");
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 8000;
 
-app.get('/', async (req, res) => {
-    const responseStream = await axios({
-        url: 'https://api.punkapi.com/v2/beers/random',
-        method: 'GET',
-        responseType: 'stream'
-    });
+app.get("/", async (req, res) => {
+  const responseStream = await axios({
+    url: "https://api.punkapi.com/v2/beers/random",
+    method: "GET",
+    responseType: "stream"
+  });
 
-    // log ----------> lower in file
-    res.type('json');
-    responseStream.data.pipe(log()).pipe(res);
+  clearConsole();
+  console.log("=================== BEGIN RESPONSE ======================");
+
+  // log ----------> lower in file
+  res.type("json");
+  responseStream.data.on('end', () => console.log('=================== END RESPONSE ======================'));
+
+  responseStream.data.pipe(log()).pipe(res);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
@@ -33,20 +38,35 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 
-
-
-
-
 const log = () => {
-    let chunksNo = 0;
+  let chunksNo = 0;
 
-    return new Transform({
-        objectMode: true,
-        transform(chunk, encoding, callback) {
-            console.log(chunk.toString());
-            console.log('=== chunk no.:', ++chunksNo);
-    
-            callback(null, chunk);
-        }
-    });
-}    
+  return new Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+      console.log(`====== BEGIN chunk no.: ${++chunksNo} ======`);
+      console.log(chunk.toString());
+      console.log(`###### END chunk no.: ${chunksNo} ######`);
+
+      callback(null, chunk);
+    }
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+const clearConsole = () => {
+  const readline = require("readline");
+  const blank = "\n".repeat(process.stdout.rows);
+  console.log(blank);
+  readline.cursorTo(process.stdout, 0, 0);
+  readline.clearScreenDown(process.stdout);
+};
